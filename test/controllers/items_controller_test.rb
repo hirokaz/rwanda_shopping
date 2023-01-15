d7219a7421d5e22bf3a5b8b5e7319b98d47a5c72
nosh1114 <nao.shiba.public@gmail.com>
2023-01-15 13:58:53 +0900

    items pages

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class ItemsControllerTest < ActionDispatch::IntegrationTest
   test "should get items" do
-    get items_index_url
+    get items_path
     assert_response :success
   end
 end
