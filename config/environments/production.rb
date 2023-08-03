7951d2e095129709be9fa085ec8007254bd3510e
芝田直生 <naoki@shibatatadashiseinoMacBook-Air.local>
2023-08-03 09:23:54 +0900

    fixed environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,4 +119,16 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port: 587,
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    user_name: 'nao.shiba.public@gmail.com',
+    password: 'cohjkmrsoqmexdoc',
+    authentication: 'login',
+    enable_starttls_auto: true
+  }
+  config.active_storage.service = :amazon
 end
